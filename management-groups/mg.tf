cd0a7146f085972251a3ca9e466a0e126a78da3b
sunny bharne <sunny.bharne@outlook.com>
2025-10-01 22:07:28 +0300

    added initial tf mg group

--- management-groups/mg.tf
+++ management-groups/mg.tf
@@ -1,7 +1,18 @@
-resource "azapi_resource" "mgTestTerraform" {
-  type = "Microsoft.Management/managementGroups@2023-04-01"
-  name = "mgTestingDescriptionName"
-  parent_id = "string"
+# resource "azapi_resource" "mg" {
+#   type      = "Microsoft.Management/managementGroups@2023-04-01"
+#   name      = "papliba-tf-tet"
+#   parent_id = "/providers/Microsoft.Management/managementGroups/papliba-bicep"
+#   body = {
+#     properties = {
+#       displayName = "papliba-tf-test"
+#     }
+#   }
+# }
+
+resource "azapi_resource" "mg" {
+  type      = "Microsoft.Management/managementGroups@2023-04-01"
+  name      = "papliba-tf"
+  parent_id = "/" # This is scope: tenant() in bicep
   body = {
     properties = {
       # details = {
@@ -9,7 +20,25 @@ resource "azapi_resource" "mgTestTerraform" {
       #     id = "string"
       #   }
       # }
-      displayName = "terraMG"
+      displayName = "papliba-tf"
     }
   }
 }
+
+# Root Management Group
+# resource "azurerm_management_group" "mg" {
+#   name         = "papliba-tf"
+#   display_name = "papliba tf"
+# }
+
+
+# # Outputs
+# output "root_mg_name" {
+#   description = "The name of the root management group."
+#   value       = azurerm_management_group.root.name
+# }
+#
+# output "root_mg_id" {
+#   description = "The resource ID of the root management group."
+#   value       = azurerm_management_group.root.id
+# }
